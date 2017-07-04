c432790bf00b78a8edab7b709179fca4a9036495
Marcell Misó <misomarcell@gmail.com>
2017-07-04 10:20:37 +0200

    Init Commit

---
+++ LoopDetector3000/LoopDetector3000/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoopDetector3000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Node n10 = new Node(10);
+            Node n9 = new Node(9, n10);
+            Node n8 = new Node(8, n9);
+            Node n7 = new Node(7, n8);
+            Node n6 = new Node(6, n7);
+            Node n5 = new Node(5, n6);
+            Node n4 = new Node(4, n5);
+            Node n3 = new Node(3, n4);
+            Node n2 = new Node(2, n3);
+            Node n1 = new Node(1, n2);
+
+            List<Node> nodes = n1.ListNodes();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                richTextBox1.Text += Environment.NewLine + nodes[i]; 
+            }
+
+            if ( n1.hasLoop() )
+            {
+                richTextBox1.Text += Environment.NewLine + " HAS LOOP ";
+            } else
+            {
+                richTextBox1.Text += Environment.NewLine + " THERE IS NO LOOP ";
+            }
+        }
+    }
+}
---
+++ LoopDetector3000/LoopDetector3000/Node.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopDetector3000
+{
+
+    class Node
+    {
+        private int Data { get; set; }
+        private Node next { get; set; }
+
+        public Node(int data, Node next)
+        {
+            Data = data;
+            this.next = next;
+        }
+
+        public Node(int data)
+        {
+            Data = data;
+            next = null;
+        }
+
+        public List<Node> ListNodes()
+        {
+
+            List<Node> nodes = new List<Node>();
+            Node n = this;
+
+            while (n != null)
+            {
+                nodes.Add(n);
+                n = n.next;
+            }
+
+            return nodes;
+        }
+
+        public Boolean hasLoop()
+        {
+            Node slow = this;
+            Node fast = next;
+
+            while ( slow != null || fast.next != null )
+            {
+                if (slow == fast)
+                {
+                    return true;
+                }
+
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "Node " + Data;
+        }
+
+    }
+}
